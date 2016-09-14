a3a8a4351ed1f4756d8f06ab2fbfa932fbad69ea
Sam Corcos <sam@corcos.io>
2016-09-14 07:20:03 -0400

    npm package for a react component part 2

--- src/PhoenixChat.jsx
+++ src/PhoenixChat.jsx
@@ -15,6 +15,68 @@ export class PhoenixChatButton extends React.Component {
   }
 }
 
+export class PhoenixChatSidebar extends React.Component {
+  constructor(props) {
+    super(props)
+    this.closeChat = this.closeChat.bind(this)
+    this.state = {
+      messages: [
+        {from: "Client", body: "Test"},
+        {from: "John", body: "Foo"},
+        {from: "Client", body: "Bar"}
+      ]
+    }
+  }
+
+  closeChat() {
+    this.props.toggleChat()
+  }
+
+  render() {
+    const list = !this.state.messages ? null : this.state.messages.map((bubble, i) => {
+      const right = bubble.from === "Client"
+      return (
+        <div style={{...style.messageWrapper, justifyContent: right ? "flex-end" : "flex-start"}}>
+          <div
+            key={i}
+            style={ right ? style.chatRight : style.chatLeft }>
+            { bubble.body }
+          </div>
+        </div>
+      )
+    })
+    return (
+      <div style={style.client}>
+        <div style={style.header}>
+          <div style={style.logo}>
+            <img
+              style={{ height: "40px", paddingRight: "5px" }}
+              alt="learnphoenix logo"
+              src="https://s3.amazonaws.com/learnphoenix-static-assets/favicons/favicon-96x96.png" />
+            <span>PhoenixChat.io</span>
+          </div>
+          <div
+            style={style.close}
+            onClick={this.closeChat}>
+            Close
+          </div>
+        </div>
+        <div style={style.chatContainer}>
+          { list }
+        </div>
+        <div style={style.inputContainer}>
+          <input
+            type="text"
+            style={style.inputBox} />
+          <div>
+            100% free by <a href="https://learnphoenix.io">PhoenixChat</a>
+          </div>
+        </div>
+      </div>
+    )
+  }
+}
+
 export class PhoenixChat extends React.Component {
   constructor(props) {
     super(props)
