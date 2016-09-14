b7739302e6273320bd3509d0708e0be5250eb03e
Sam Corcos <sam@corcos.io>
2016-09-14 07:15:02 -0400

    npm package for a react component part 1

---
+++ src/style.js
@@ -0,0 +1,29 @@
+export const style = {
+  chatButton: {
+    position: "fixed",
+    right: "20px",
+    bottom: "20px",
+    background: "rgb(58, 155, 207)",
+    color: "white",
+    zIndex: "1000",
+    height: "60px",
+    width: "60px",
+    border: "1px solid #ccc",
+    borderRadius: "50%",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
+    cursor: "pointer",
+    boxShadow: "2px 4px 2px -2px rgba(0,0,0,0.3)"
+  },
+  chatImage: {
+    display: "block",
+    height: "40px",
+    width: "40px",
+    filter: "invert(100%)",
+    WebkitFilter: "invert(100%)",
+    MozFilter: "invert(100%)"
+  }
+}
+
+export default style
